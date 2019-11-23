3561394c236baf42ae2192ad410aad43a4d1564c
Rafel Olmos <rafelolmos@MacBook-Air-de-Rafel.local>
2019-11-23 13:54:19 +0100

    setup router boilerplate

--- rafel-cra-boilerplate/src/App.js
+++ rafel-cra-boilerplate/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import About from './pages/About';
 
 
 function App() {
+  
   return (
+    <header>
+      <Route>
+        <Switch>
+          <Route path="/"><Home /></Route>
+          <Route path="/"><About /></Route>
+        </Switch>
+      </Route>
+    </header>
     <div>
       <div>Create React App BoilerPlate</div>
     </div>
-  );
+  )
 }
 
 export default App;
