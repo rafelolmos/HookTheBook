0e8b72eb2806146c70d761faffd6832b72dba7d4
Rafel Olmos <rafelolmos@MacBook-Air-de-Rafel.local>
2019-12-13 19:28:48 +0100

    friends books list added

--- src/pages/MyFriendsBooks/MyFriendsBooks.js
+++ src/pages/MyFriendsBooks/MyFriendsBooks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAllRealTime } from '../../services/database';
+import { getAll } from '../../services/database';
 import { useSelector} from 'react-redux';
 
 import './MyBooksFriends.scss'
@@ -11,28 +11,21 @@ import CardBook from '../../components/CardBook/CardBook';
 const MyFriendsBooks = () => {
 
     const user = useSelector((state)=> state.user);
+    console.log('user: ', user);
 
     const [bookList, setBookList] = useState([])
-
-    
+    console.log('bookList: ', bookList);
+        
     useEffect(() => {
+        
+        getAll('books').then(result=>{
+            const list = result.filter(function(elem){
+                console.log('elem: ', elem);
+                return elem.user !== user.id;
+            })
+            setBookList(list)
+        })
 
-        getAllRealTime({
-            collection: 'books',
-            filters: { field: 'user', condition: '==', value: user.id }, //ha de ser differnt de l'usuari
-            order: 'timestamp',
-            callback: (collectionData) => {
-            const results = [];
-            collectionData.forEach((document) => {
-                const data = document.data();
-                const addedBookDate = new Date(data.timestamp);
-                data.date = addedBookDate.toLocaleDateString();
-                data.time = addedBookDate.toLocaleTimeString();
-                results.push(data);
-            });
-            setBookList(results);
-            }
-        });
       }, [user])
 
     return ( 
